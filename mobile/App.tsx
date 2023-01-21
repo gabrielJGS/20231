7553c0324b9f3e1509ceb0cb77fa5544853421dd
gabrielJGS <gabrielftjose@gmail.com>
2023-01-21 14:45:07 -0300

    feat: mobile home with header and habitday components using dayjs, nativewind and react-native-svg

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import "./src/lib/dayjs";
+import { StatusBar } from "react-native";
 import {
   useFonts,
   Inter_400Regular,
@@ -6,8 +7,8 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold,
 } from "@expo-google-fonts/inter";
-import {Loading} from './src/components/Loading';
-
+import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,27 +17,14 @@ export default function App() {
     Inter_700Bold,
     Inter_800ExtraBold,
   });
-  if (!fontsLoaded){
-    return <Loading></Loading>
-  };
+  if (!fontsLoaded) {
+    return <Loading></Loading>;
+  }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar barStyle="light-content" backgroundColor='transparent' translucent/>
-    </View>
+    <>
+      <Home />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#09090A",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text:{
-    color: '#FFF',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
