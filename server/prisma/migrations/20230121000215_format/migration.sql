1f4d7cceb7c3917bcd6e06610c9dbac165442cd8
gabrielJGS <gabrielftjose@gmail.com>
2023-01-21 11:34:03 -0300

    server aula 2: prisma seed, dayjs e zod

---
+++ server/prisma/migrations/20230121000215_format/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - Added the required column `habitId` to the `habit_week_days` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_habit_week_days" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "habit_id" TEXT NOT NULL,
+    "week_day" INTEGER NOT NULL,
+    "habitId" TEXT NOT NULL,
+    CONSTRAINT "habit_week_days_habitId_fkey" FOREIGN KEY ("habitId") REFERENCES "habits" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_habit_week_days" ("habit_id", "id", "week_day") SELECT "habit_id", "id", "week_day" FROM "habit_week_days";
+DROP TABLE "habit_week_days";
+ALTER TABLE "new_habit_week_days" RENAME TO "habit_week_days";
+CREATE UNIQUE INDEX "habit_week_days_habit_id_week_day_key" ON "habit_week_days"("habit_id", "week_day");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
